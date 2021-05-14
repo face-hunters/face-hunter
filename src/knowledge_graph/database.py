984962a75a37c68f14eac6966edc31f5e6216102
Tim Grams <t.grams339@gmail.com>
2021-05-14 15:18:37 +0200

    Basic rdf-generation class

--- src/knowledge_graph/database.py
+++ src/knowledge_graph/database.py
@@ -2,14 +2,12 @@ import logging
 import os
 
 from src.utils.utils import check_path_exists
-from rdflib import Graph, URIRef, Namespace, Literal, XSD
-from rdflib.namespace import DC, RDF, FOAF
+from rdflib import Graph, URIRef, Literal
+from rdflib.namespace import DC, RDF
 from rdflib.plugins.sparql import prepareQuery
 
 LOGGER = logging.getLogger('l')
-
-TEMPORAL = Namespace('http://swrl.stanford.edu/ontologies/built-ins/3.3/temporal.owl')
-MPEG7 = Namespace('http://purl.org/ontology/mpeg7/')
+HOME_URI = 'http://example.org/'
 
 
 class Database(object):
@@ -25,49 +23,40 @@ class Database(object):
             self.graph.parse('models/store', format='n3')
         else:
             self.graph.bind('dc', DC)
-            self.graph.bind('mpeg7', MPEG7)
-            self.graph.bind('temporal', TEMPORAL)
-            self.graph.bind('foaf', FOAF)
 
     def save(self, path: str = 'models'):
         check_path_exists(path)
         self.graph.serialize(os.path.join(path, 'store'), format='n3')
 
     def create_video(self, youtube_id, title):
-        video_uri = URIRef(f'http://localhost/{youtube_id}')
-        self.graph.add((video_uri, RDF['type'], MPEG7['Video']))
+        video_uri = URIRef(f'{HOME_URI}{youtube_id}')
+        self.graph.add((video_uri, RDF['type'], DC['MovingImage']))
         self.graph.add((video_uri, DC['identifier'], Literal(f'http://www.youtube.com/watch?v={youtube_id}')))
         self.graph.add((video_uri, DC['title'], Literal(title)))
         self.save()
 
-    def add_entity_to_video(self, entity, youtube_id, time_range):
-        video_uri = URIRef(f'http://localhost/{youtube_id}')
+    def add_entity_to_video(self, entity, youtube_id):
+        video_uri = URIRef(f'{HOME_URI}{youtube_id}')
         entity_uri = URIRef(f'http://dbpedia.org/resource/{entity}')
-        scene_uri = URIRef('http://localhost/scene/2')
 
-        self.graph.add((scene_uri, RDF['type'], DC['PeriodOfTime']))
-        self.graph.add((video_uri, DC['hasPart'], scene_uri))
-        self.graph.add((scene_uri, TEMPORAL['hasStartTime'], Literal(time_range[0], datatype=XSD['time'])))
-        self.graph.add((scene_uri, TEMPORAL['hasFinishTime'], Literal(time_range[1], datatype=XSD['time'])))
-        self.graph.add((scene_uri, FOAF['depicts'], entity_uri))
+        self.graph.add((video_uri, DC['contributor'], entity_uri))
         self.save()
 
     def video_is_in(self, youtube_id):
-        return (URIRef(f'http://localhost/{youtube_id}'), RDF['type'], MPEG7['Video']) in self.graph
+        return (URIRef(f'{HOME_URI}{youtube_id}'), RDF['type'], DC['MovingImage']) in self.graph
 
     def get_videos_of_entity(self, entity):
         q = prepareQuery(
             '''
             SELECT ?url ?title 
             WHERE {
-                ?video a mpeg7:Video; 
+                ?video a dc:MovingImage; 
+                    dc:contributor ?entity;
                     dc:identifier ?url; 
-                    dc:title ?title; 
-                    dc:hasPart ?part. 
-                ?part foaf:depicts ?entity
+                    dc:title ?title.
             }
             ''',
-            initNs={'mpeg7': MPEG7, 'dc': DC, 'foaf': FOAF}
+            initNs={'dc': DC}
         )
         entity_uri = URIRef(f'http://dbpedia.org/resource/{entity}')
         results = []
