d9979448d9489a0fefa5479703d83a72b9fbf042
Tim Grams <t.grams339@gmail.com>
2021-05-31 23:15:12 +0200

    Added virtouso-support and federated sparql-query

---
+++ src/knowledge_graph/graph.py
@@ -0,0 +1,139 @@
+import logging
+from datetime import timedelta
+from rdflib import URIRef, Literal
+from rdflib.namespace import DC, RDF, Namespace, FOAF, XSD
+
+from src.knowledge_graph.memory_store import MemoryStore
+from src.knowledge_graph.virtuoso_store import VirtuosoStore
+from src.utils.utils import get_config
+
+LOGGER = logging.getLogger('graph')
+CONFIG = get_config()
+
+HOME_URI = CONFIG['rdf']['uri']
+
+MPEG7 = Namespace('http://purl.org/ontology/mpeg7/')
+VIDEO = Namespace('http://purl.org/ontology/video/')
+TEMPORAL = Namespace('http://swrl.stanford.edu/ontologies/builtins/3.3/temporal.owl')
+DBO = Namespace('http://dbpedia.org/ontology/')
+DBR = Namespace('http://dbpedia.org/resource/')
+
+
+class Graph(object):
+    """
+    Store
+
+    Links new videos with their entities in the knowledge graph and allows to look up user queries.
+    The data format is based on the paper: https://www.tandfonline.com/doi/full/10.1080/24751839.2018.1437696
+
+    """
+    def __init__(self, storage_type: str = 'memory',
+                 memory_path: str = 'models/store',
+                 virtuoso_url: str = None,
+                 virtuoso_username: str = None,
+                 virtuoso_password: str = None):
+        if storage_type == 'memory':
+            self.store = MemoryStore(memory_path)
+        elif storage_type == 'virtuoso':
+            self.store = VirtuosoStore(virtuoso_url, virtuoso_username, virtuoso_password)
+        else:
+            raise Exception('Unknown storage type')
+
+    def insert_video(self, youtube_id: str, title: str):
+        """ Creates the rdf triples for a new video.
+
+        Parameters
+        ----------
+        youtube_id: str
+            Id of a youtube video to be linked.
+            For example a4T5ylNQk6g for https://www.youtube.com/watch?v=a4T5ylNQk6g.
+        title: str
+            The title of the youtube video.
+        """
+        video_uri = URIRef(f'{HOME_URI}{youtube_id}')
+
+        self.store.insert((video_uri, RDF['type'], MPEG7['Video']))
+        self.store.insert((video_uri, DC['identifier'], Literal(f'http://www.youtube.com/watch?v={youtube_id}')))
+        self.store.insert((video_uri, DC['title'], Literal(title)))
+        self.store.commit()
+
+    def insert_scene(self, entities: list, youtube_id: str, start_time: timedelta, end_time: timedelta):
+        """ Creates the link between an entity and a video from youtube.
+
+        Parameters
+        ----------
+        entities: list
+            Names of the occurring entities.
+        youtube_id: str
+            Id of a youtube video to be linked.
+            For example a4T5ylNQk6g for https://www.youtube.com/watch?v=a4T5ylNQk6g.
+        start_time: timedelta
+            Start time of the scene in the respective video.
+        end_time: timedelta
+            End time of the scene in the respective video.
+        """
+        video_uri = URIRef(f'{HOME_URI}{youtube_id}')
+        scene_uri = URIRef(f'{HOME_URI}{youtube_id}#t={str(start_time)},{str(end_time)}')
+
+        self.store.insert((scene_uri, RDF['type'], VIDEO['Scene']))
+        self.store.insert((scene_uri, VIDEO['sceneFrom'], video_uri))
+        self.store.insert((scene_uri, VIDEO['temporalSegmentOf'], video_uri))
+        self.store.insert((scene_uri, TEMPORAL['hasStartTime'], Literal(str(start_time), datatype=XSD['dateTime'])))
+        self.store.insert(
+            (scene_uri, TEMPORAL['duration'], Literal(str(end_time - start_time), datatype=XSD['duration'])))
+        self.store.insert((scene_uri, TEMPORAL['hasFinishTime'], Literal(str(end_time), datatype=XSD['dateTime'])))
+        for entity in entities:
+            entity_uri_dbpedia = URIRef(f'http://dbpedia.org/resource/{entity.replace(" ", "_")}')
+            self.store.insert((scene_uri, FOAF['depicts'], entity_uri_dbpedia))
+        self.store.commit()
+
+    def get_videos_with_entity_name(self, entity: str):
+        """ Returns all videos for an entity
+
+        Parameters
+        ----------
+        entity: str
+            Name of the entity.
+
+        Returns
+        ----------
+        List
+            Returns a list of the videos in which a entity occurs. Format: [[<link>, <title>], ...]
+        """
+        query = ('SELECT DISTINCT ?link ?title'
+                 'HERE {'
+                 '?scene a video:Scene ;'
+                 f'foaf:depicts <http://dbpedia.org/resource/{entity.replace(" ", "_")}> ;'
+                 'video:sceneFrom ?video .'
+                 '?video a mpeg7:Video ;'
+                 'dc:identifier ?link '
+                 'dc:title ?title .'
+                 '}')
+        return self.store.query(query)
+
+    def get_videos_with_dbpedia_query(self):
+        """ Returns videos for a user specific query.
+            For example: Videos with actors born before 1970.
+
+        Returns
+        ----------
+        List
+            Returns a list of the videos in which a entity occurs. Format: [[<link>, <title>, <entity>], ...]
+        """
+        query = '''
+        select distinct ?title ?link ?entity 
+        where { 
+            ?scene a video:Scene; 
+                foaf:depicts ?entity; 
+                video:sceneFrom ?video. 
+            ?video dc:identifier ?link;
+                    dc:title ?title.
+            
+            service <http://dbpedia.org/sparql> { 
+                ?entity dbo:birthDate ?date 
+            } 
+            
+            filter (?date < "19700101"^^xsd:date) 
+        }
+        '''
+        return self.store.query(query)
---
+++ src/knowledge_graph/virtuoso_store.py
@@ -0,0 +1,72 @@
+import logging
+
+from SPARQLWrapper import SPARQLWrapper, JSON, DIGEST, POST
+from pandas import json_normalize
+
+LOGGER = logging.getLogger('virtuoso-store')
+
+
+class VirtuosoStore(object):
+    """
+    Virtuoso Store
+
+    Implementation to store and query rdf-triples in a Virtuoso instance
+
+    """
+    def __init__(self, conn_url: str = 'http://localhost:8890/sparql-auth',
+                 graph: str = 'http://localhost:8890/DAV/',
+                 username: str = 'dba',
+                 password: str = 'dba'):
+        self.graph = graph
+        self.endpoint = SPARQLWrapper(conn_url)
+        self.endpoint.setHTTPAuth(DIGEST)
+        self.endpoint.setCredentials(username, password)
+        self.endpoint.setReturnFormat(JSON)
+        self.insert_query = ''
+
+    def insert(self, triple: tuple):
+        """ Add a triple to the knowledge graph
+
+        Parameters
+        ----------
+        triple: tuple
+            The triple to save.
+        """
+        self.insert_query += f' <{str(triple[0])}> <{str(triple[1])}>'
+        if type(triple[2]).__name__ == 'Literal':
+            self.insert_query += f' "{str(triple[2])}" .'
+        else:
+            self.insert_query += f' <{str(triple[2])}> .'
+
+    def commit(self):
+        """ Make the changes persistent and available
+        """
+        self.endpoint.setMethod(POST)
+        query = ('INSERT DATA {'
+                 f'GRAPH <{self.graph}>'
+                 ' {'
+                 f'{self.insert_query}'
+                 '}'
+                 '}')
+        self.query(query)
+
+    def query(self, query: str):
+        """ Execute a SPARQL query against a Virtuoso endpoint
+
+        Parameters
+        ----------
+        query: str
+            A valid SPARQL query.
+
+        Returns
+        ----------
+        results: List
+            Returns a list of lists with the queried properties. Format: [[<property1>, <property2>, ...], ...]
+        """
+        self.endpoint.setQuery(query)
+        query_results = json_normalize(self.endpoint.query().convert()['results']['bindings'])
+        value_columns = [col for col in query_results if col.endswith('.value')]
+        results = []
+        for index, row in query_results.iterrows():
+            results.append([row[col] for col in value_columns])
+        return results
