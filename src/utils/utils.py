332a69db100f2a955768e00383ad6404936869cd
Honglin-Li <catyynet@gmail.com>
2021-06-07 23:56:56 +0200

    add face_number() to utils

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -2,6 +2,9 @@ import logging
 import os
 import yaml
 
+from mtcnn import MTCNN
+import cv2
+
 LOGGER = logging.getLogger('utils')
 
 
@@ -34,3 +37,24 @@ def get_config(path: str = 'config.yaml'):
     with open(path) as f:
         config = yaml.safe_load(f)
     return config
+
+
+def face_number(img, detector=MTCNN()):
+    """ caculate how many faces in img, for download thumbnails use
+
+    if you have to detect many image, you can create a detector then pass to this function:
+      detector = MTCNN()
+      for img in img_list:
+        face_number = face_number('img.jpg', detector)
+
+    params:
+      img: can be img object or img path
+  
+    return:
+      face number in the img
+    """
+    if isinstance(img, str):
+        img = cv2.cvtColor(cv2.imread(img), cv2.COLOR_BGR2RGB)
+
+    faces = detector.detect_faces(img)
+    return len(faces)
