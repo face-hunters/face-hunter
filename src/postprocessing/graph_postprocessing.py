9f46b4740327dd2db75f8a809bb3f8348fee557c
Tim Grams <t.grams339@gmail.com>
2021-08-01 14:28:58 +0200

    Added costum threshold to scene extraction

--- src/postprocessing/graph_postprocessing.py
+++ src/postprocessing/graph_postprocessing.py
@@ -1,32 +1,42 @@
 import logging
 from datetime import timedelta
+import numpy as np
 
 LOGGER = logging.getLogger('graph-postprocessing')
 
 
-def extract_scenes(recognitions: list, timestamps: list):
+def extract_scenes(recognitions: list, timestamps: list, frame_threshold: int = 3):
     assert len(recognitions) == len(timestamps), 'recognitions do not fit timestamps'
 
     scenes = []
     current_scene = None
-    for frame, entities in enumerate(recognitions[:-1], 2):
-        if set(entities) != set(recognitions[frame - 1]) and set(entities) != set(recognitions[frame - 2]):
-            if current_scene is not None:
-                scenes.append(current_scene.set_end(timestamps[frame]))
-        if set(entities) == set(recognitions[frame - 1]) and set(entities) == set(recognitions[frame - 2]):
-            if current_scene is None or set(current_scene.names[0]) != set(entities):
-                current_scene = Scene(entities).set_start(timestamps[frame])
-        if current_scene is not None and frame == (len(recognitions)):
-            scenes.append(current_scene.set_end(timestamps[frame - 1]))
+    for frame, entities in enumerate(recognitions):
+        if frame - 2 < 0:
+            continue
+
+        if current_scene is not None and not np.any([np.all(np.char.equal(np.sort(pred), current_scene.names[0]))
+                                                     for pred in recognitions[frame - (frame_threshold - 1):frame + 1]]):
+            scenes.append(current_scene.set_end(timestamps[frame]))
+            current_scene = None
+
+        if current_scene is None and np.all([np.char.equal(np.sort(pred), np.sort(entities))
+                                             for pred in recognitions[frame - (frame_threshold - 1):frame]]):
+            current_scene = Scene(entities).set_start(timestamps[frame])
+
+        if current_scene is not None and frame == (len(recognitions) - 1):
+            scenes.append(current_scene.set_end(timestamps[frame]))
     return scenes
 
 
 class Scene(object):
 
     def __init__(self, names: list):
-        self.names = names,
+        self.names = np.sort(names),
         self.start = None
         self.end = None
+
+    def set_names(self, names: list):
+        self.names = np.sort(names)
 
     def set_start(self, milliseconds):
         self.start = timedelta(milliseconds=milliseconds),
@@ -35,3 +45,6 @@ class Scene(object):
     def set_end(self, milliseconds):
         self.end = timedelta(milliseconds=milliseconds),
         return self
+
+    def __repr__(self):
+        return f'<{self.names[0]}>: {self.start[0]}, {self.end[0]}'
