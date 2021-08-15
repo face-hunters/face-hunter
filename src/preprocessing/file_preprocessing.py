3ca8ce9879206b04cdb5ef8e5f221d25941f02c3
bbbbtong <81519698+bbbbtong@users.noreply.github.com>
2021-08-16 05:28:11 +0800

    Implement parallelized name normalization

--- src/preprocessing/file_preprocessing.py
+++ src/preprocessing/file_preprocessing.py
@@ -3,6 +3,24 @@ import wikipedia
 
 LOGGER = logging.getLogger('file-preprocessing')
 
+def norm(name, j):
+  LOGGER.info(f'process{j} running')
+  try:
+    wikipedia.set_lang("en") 
+    search_list = wikipedia.search(name, results=1)
+    if search_list:
+      return (search_list[0],j,1)
+    else:
+      wikipedia.set_lang("de")
+      search_list = wikipedia.search(name, results=1)
+      if search_list:
+        return (search_list[0],j,1)
+      else:
+        return (name,j,0)
+  except:
+    LOGGER.info(f'{name} error')
+    return (name,j,0)
+
 
 def name_norm(name_list):
     """ Normalize name based on Wikipedia search
@@ -17,24 +35,24 @@ def name_norm(name_list):
     list
         The name list has been normalized
     """
-    wiki_name = []
-    i = 0
-    try:
-        for name in name_list:
-            wikipedia.set_lang("en")
-            search_list = wikipedia.search(name, results=1)
-            if search_list:
-                wiki_name.append(search_list[0])
-            else:
-                wikipedia.set_lang("de")
-                search_list = wikipedia.search(name, results=1)
-                if search_list:
-                    wiki_name.append(search_list[0])
-                else:
-                    i = i + 1
-                    wiki_name.append('missing')
-    except:
-        LOGGER.info(f'{name} error')
-        wiki_name.append('missing')
-    LOGGER.info(f'{i} people cant be found in wikipedia')
+    def add_name(result):
+      global wiki_name
+      global missing_name 
+      name,j,search = result
+      wiki_name[j]=name
+      if not search:
+        missing_name.append(name)
+    global wiki_name
+    wiki_name = [None]*len(name_list)
+    global missing_name
+    missing_name = []
+    j = 0
+    pool = mp.Pool(mp.cpu_count())
+    for name in name_list:
+      pool.apply_async(norm, args=(name, j), callback=add_name)
+      j = j + 1
+    pool.close()
+    pool.join()
+    LOGGER.info(f'{len(missing_name)} people cant be found in wikipedia, They are:')
+    LOGGER.info(missing_name)
     return wiki_name
