adf5c3188e8805746c8bfbbacba73c956343e571
Tim Grams <t.grams339@gmail.com>
2021-08-28 01:43:57 +0200

    removed cmake

--- setup.py
+++ setup.py
@@ -9,7 +9,6 @@ setup(
     license='',
     install_requires=[
         "wget~=3.2",
-        "cmake",
         "opencv-python~=4.5.1.48",
         "numpy~=1.19.2",
         "face_recognition~=1.3.0",
