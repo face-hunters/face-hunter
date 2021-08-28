99f2c27cbaba92d84de4714a5c6f6ae020ecee6f
Tim Grams <t.grams339@gmail.com>
2021-08-28 11:01:28 +0200

    Added mock to docs config

--- docs/conf.py
+++ docs/conf.py
@@ -43,6 +43,9 @@ templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
+autodoc_mock_imports = ['dlib']
+
+
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
