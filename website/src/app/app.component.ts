e92e3661f3a7a06a14b74bf1ef78521d001489fc
Tim Grams <t.grams339@gmail.com>
2021-09-08 15:30:35 +0100

    Revert "Revert "Updated website""
    This reverts commit 1b7798a7

--- website/src/app/app.component.ts
+++ website/src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  
+export class AppComponent implements OnInit {
+
+  value: any;
+
+  videos: any[] = [];
+
+  constructor(){
+    this.value = '';
+    this.videos = []
+  }
+
+  ngOnInit() {
+    const tag = document.createElement('script');
+    tag.src = 'https://www.youtube.com/iframe_api';
+    document.body.appendChild(tag);
+  }
+
+  get_videos_of_celebritiy() {
+    this.videos.push({id: 'LKvlfxVC210', start: 3, end: 15, duration: 12});
+    this.videos.push({id: 'N_gD9-Oa0fg', start: 3, end: 20, duration: 17});
+  }
+
+  reset() {
+    this.videos = [];
+  }
+
 }
--- website/src/app/app.module.ts
+++ website/src/app/app.module.ts
@@ -5,6 +5,11 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {YouTubePlayerModule} from "@angular/youtube-player";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatIconModule} from "@angular/material/icon";
+import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
   declarations: [
@@ -13,7 +18,12 @@ import {MatToolbarModule} from '@angular/material/toolbar';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    YouTubePlayerModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
