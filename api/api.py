1bb08e80f04579bf5027e7118679d90f1d1b3c22
Tim Grams <t.grams339@gmail.com>
2021-09-27 11:55:07 +0100

    api syntax correction

--- api/api.py
+++ api/api.py
@@ -10,23 +10,23 @@ from flask_api import FlaskApi
 #  todo: from config
 BY = 'frame'
 FRAME_THRESHOLD = 7
-STORAGE_TYPE = 'memory'
-MEMORY_PATH = 'models/store'
+STORAGE_TYPE = 'virtuoso'
+MEMORY_PATH = ''
 VIRTUOSO_URL = 'http://localhost:8890/sparql-auth'
 VIRTUOSO_GRAPH = 'http://localhost:8890/DAV/'
 VIRTUOSO_USERNAME = 'dba'
 VIRTUOSO_PASSWORD = 'dba'
 DBPEDIA_CSV = None
-WIKIDATA_CSV = 'data/thumbnails/Thumbnails_links.csv'
+WIKIDATA_CSV = '../data/thumbnails/Thumbnails_links.csv'
 
 THUMBNAIL_LIST = None
-THUMBNAILS_PATH = 'data/thumbnails/thumbnails'
+THUMBNAILS_PATH = '../data/thumbnails'
 IMG_WIDTH = 500
 DISTANCE_THRESHOLD = 0.72
 ENCODER_NAME = 'Facenet'
-LABELS_PATH = 'data/embeddings/labels_facenet.pickle'
-EMBEDDINGS_PATH = 'data/embeddings/embeddings_facenet.pickle'
-INDEX_PATH = 'data/embeddings/index.bin'
+LABELS_PATH = '../data/embeddings/labels_facenet.pickle'
+EMBEDDINGS_PATH = '../data/embeddings/embeddings_facenet.pickle'
+INDEX_PATH = '../data/embeddings/index.bin'
 
 app = Flask(__name__)
 run_with_ngrok(app)
@@ -48,31 +48,36 @@ api = FlaskApi(
     LABELS_PATH,
     EMBEDDINGS_PATH,
     INDEX_PATH
-    )
+)
+
 
 @app.route('/api/youtube/<id>', methods=['GET'])
 def recognize_youtube_video(id):
     result = api.recognize_youtube_video(id, BY, FRAME_THRESHOLD)
     return jsonify({'success': True, 'result': result})
 
+
 @app.route('/api/query', methods=['POST'])
 def get_videos_by_sparql():
     sparql = None
 
     if request.headers['Content-Type'] == 'application/json':
-      sparql = json.loads(request.data).get('sparql')
-      result = api.get_videos_by_sparql(sparql)
-      return jsonify({'success': True, 'result': result})
-      
+        sparql = json.loads(request.data).get('sparql')
+        result = api.get_videos_by_sparql(sparql)
+        return jsonify({'success': True, 'result': result})
+
     return jsonify({'success': False, 'result': 'wrong request type'})
+
 
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
+
 @app.errorhandler(500)
 def iternal_error(error):
     return make_response(jsonify({'error': 'Internal Server Error'}), 500)
+
 
 if __name__ == '__main__':
     app.run()
--- api/flask_api.py
+++ api/flask_api.py
@@ -25,28 +25,31 @@ class FlaskApi(object):
                  virtuoso_graph: str = None,
                  virtuoso_username: str = None,
                  virtuoso_password: str = None,
-                 dbpedia_csv: str = 'data/thumbnails/dbpedia_thumbnails/Thumbnails_links.csv',
-                 wikidata_csv: str = 'data/thumbnails/wikidata_thumbnails/Thumbnails_links.csv',
+                 dbpedia_csv: str = '../data/thumbnails/dbpedia_thumbnails/Thumbnails_links.csv',
+                 wikidata_csv: str = '../data/thumbnails/wikidata_thumbnails/Thumbnails_links.csv',
                  # parameters for FaceRecognition
                  thumbnail_list: list = None,
-                 thumbnails_path='data/thumbnails/thumbnails',
-                 img_width = 500,
-                 distance_threshold = 0.4,
-                 encoder_name = 'Facenet',
-                 labels_path = 'data/embeddings/labels_facenet.pickle',
-                 embeddings_path = 'data/embeddings/embeddings_facenet.pickle',
-                 index_path = 'data/embeddings/index.bin'):
-      """ Instantiate ApproximateKNearestNeighbors， FaceRecognition and Graph """
-      self.recognizer_model = ApproximateKNearestNeighbors(distance_threshold=distance_threshold, index_path=index_path)
-      self.face_recognition = FaceRecognition(thumbnail_list, thumbnails_path, img_width, distance_threshold, encoder_name, labels_path, embeddings_path)
-      self.graph = Graph(storage_type, memory_path, virtuoso_url, virtuoso_graph, virtuoso_username, virtuoso_password, dbpedia_csv, wikidata_csv)
+                 thumbnails_path='../data/thumbnails/thumbnails',
+                 img_width=500,
+                 distance_threshold=0.4,
+                 encoder_name='Facenet',
+                 labels_path='../data/embeddings/labels_facenet.pickle',
+                 embeddings_path='../data/embeddings/embeddings_facenet.pickle',
+                 index_path='../data/embeddings/index.bin'):
+        """ Instantiate ApproximateKNearestNeighbors， FaceRecognition and Graph """
+        self.recognizer_model = ApproximateKNearestNeighbors(distance_threshold=distance_threshold,
+                                                             index_path=index_path)
+        self.face_recognition = FaceRecognition(thumbnail_list, thumbnails_path, img_width, distance_threshold,
+                                                encoder_name, labels_path, embeddings_path)
+        self.graph = Graph(storage_type, memory_path, virtuoso_url, virtuoso_graph, virtuoso_username,
+                           virtuoso_password, dbpedia_csv, wikidata_csv)
 
     def recognize_local_video(self, path):
-      return self.face_recognition.recognize_video(path, self.recognizer_model)
-      #return self.face_recognition.recognize_video(path)
-    
+        return self.face_recognition.recognize_video(path, self.recognizer_model)
+        # return self.face_recognition.recognize_video(path)
+
     def recognize_youtube_video(self, identifier, by='frame', frame_threshold=7):
-      """
+        """
         if video is in KG, query from KG
         if not, download and recognize video, then insert video to KG
 
@@ -59,22 +62,26 @@ class FlaskApi(object):
         ----------
         result: [[entity, start, end],...]
       """
-      result = []
-      if self.graph.video_exists(identifier):
-        result = [scene[1:] for scene in self.graph.get_scenes_from_video(identifier)]  # [scene, entity, start, end]
+        result = []
+        if self.graph.video_exists(identifier):
+            result = [scene[1:] for scene in
+                      self.graph.get_scenes_from_video(identifier)]  # [scene, entity, start, end]
+            return result
+
+        path_to_video = download_youtube_video(f'https://www.youtube.com/watch?v={identifier}', tempfile.gettempdir())
+
+        recognized_entities, frame_wise_entities, timestamps = self.face_recognition.recognize_video(path_to_video,
+                                                                                                     self.recognizer_model,
+                                                                                                     by)
+
+        self.graph.insert_video(identifier, os.path.split(path_to_video)[1])
+        scenes = extract_scenes(frame_wise_entities, timestamps, frame_threshold)
+        for scene in scenes:
+            result.append([scene.names[0].tolist(), str(scene.start[0]).split('.')[0],
+                           str(scene.end[0]).split('.')[0]])  # accurate to second
+            graph.insert_scene(scene.names[0], identifier, scene.start[0], scene.end[0])
+
         return result
 
-      path_to_video = download_youtube_video(f'https://www.youtube.com/watch?v={identifier}', tempfile.gettempdir())
-
-      recognized_entities, frame_wise_entities, timestamps = self.face_recognition.recognize_video(path_to_video, self.recognizer_model, by)
-
-      graph.insert_video(identifier, os.path.split(path_to_video)[1])
-      scenes = extract_scenes(frame_wise_entities, timestamps, frame_threshold)
-      for scene in scenes:
-        result.append([scene.names[0].tolist(), str(scene.start[0]).split('.')[0], str(scene.end[0]).split('.')[0]])  # accurate to second
-        graph.insert_scene(scene.names[0], identifier, scene.start[0], scene.end[0])
-      
-      return result
-
     def get_videos_by_sparql(self, sparql):
-      return self.graph.get_videos_with_filters(sparql)
+        return self.graph.get_videos_with_filters(sparql)
