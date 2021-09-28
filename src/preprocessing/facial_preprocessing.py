59f2a9023143aeef41f34367288303e18ec30f1a
Tim Grams <t.grams339@gmail.com>
2021-09-28 11:33:40 +0100

    Refactored preprocessing

--- src/preprocessing/facial_preprocessing.py
+++ src/preprocessing/facial_preprocessing.py
@@ -1,9 +1,21 @@
 import numpy as np
 import cv2
 from tensorflow.keras.preprocessing.image import img_to_array
+import matplotlib.pyplot as plt
 
 
-def face_alignment(img, shape, keypoints, blank=0.3):
+def face_alignment(img, shape, keypoints, blank=0.3, show=False):
+    """ Aligns an image according to keypoints.
+
+    Args:
+        img (cv2 Image): The original image.
+        shape(tuple): Tuple of two floats that show the target image shape.
+        keypoints (dict): The coordinates of the detected face. (left_eye, right_eye, mouth_left, mouth_right).
+        blank (float): Scaling Parameter.
+        show (bool): Whether  the aligned Image should be displayed or not.
+    Returns:
+        img_pixels (cv2 Image): Aligned Image
+    """
     left_eye = keypoints['left_eye']
     right_eye = keypoints['right_eye']
     mouth_left = keypoints['mouth_left']
@@ -34,11 +46,12 @@ def face_alignment(img, shape, keypoints, blank=0.3):
 
     aligned_face = cv2.warpAffine(img, M, shape, flags=cv2.INTER_CUBIC)
 
-    # plt.imshow(aligned_face)
-    # plt.show()
+    if show:
+        plt.imshow(aligned_face)
+        plt.show()
 
     # input layer shape
-    img_pixels = img_to_array(aligned_face)
+    # img_pixels = img_to_array(aligned_face)
     img_pixels = np.expand_dims(aligned_face, axis=0)
     img_pixels = img_pixels / 255
 
--- src/preprocessing/file_preprocessing.py
+++ src/preprocessing/file_preprocessing.py
@@ -4,54 +4,62 @@ import multiprocessing as mp
 
 LOGGER = logging.getLogger('file-preprocessing')
 
+
 def norm(name, j):
-  LOGGER.info(f'process{j} running')
-  try:
-    wikipedia.set_lang("en") 
-    search_list = wikipedia.search(name, results=1)
-    if search_list:
-      return (search_list[0],j,1)
-    else:
-      wikipedia.set_lang("de")
-      search_list = wikipedia.search(name, results=1)
-      if search_list:
-        return (search_list[0],j,1)
-      else:
-        return (name,j,0)
-  except:
-    LOGGER.info(f'{name} error')
-    return (name,j,0)
+    """ Looks up the Wikipedia name for a dataset name.
+
+    Args:
+        name (str): The name to normalize.
+        j (int): Helper variable to identify the  origin process.
+    Returns:
+        name (str): Normalized name.
+        j (int): Helper variable to identify the  origin process.
+        Number (int): 1 if the lookup succeeded, 0 otherwise.
+    """
+    LOGGER.info(f'process{j} running')
+    try:
+        wikipedia.set_lang("en")
+        search_list = wikipedia.search(name, results=1)
+        if search_list:
+            return search_list[0], j, 1
+        else:
+            wikipedia.set_lang("de")
+            search_list = wikipedia.search(name, results=1)
+            if search_list:
+                return search_list[0], j, 1
+            else:
+                return name, j, 0
+    except:
+        LOGGER.info(f'{name} error')
+        return name, j, 0
 
 
 def name_norm(name_list):
-    """ Normalize name based on Wikipedia search
+    """ Normalize name based on Wikipedia search.
 
-    Parameters
-    ----------
-    folder: list, default = None
-        The name list needs to be normalized
-
-    Returns
-    ----------
-    list
-        The name list has been normalized
+    Args:
+        name_list (list): List of names to be normalized.
+    Returns:
+        wiki_name (list): List of normalized names.
     """
+
     def add_name(result):
-      global wiki_name
-      global missing_name 
-      name,j,search = result
-      wiki_name[j]=name
-      if not search:
-        missing_name.append(name)
+        global wiki_name
+        global missing_name
+        name, j, search = result
+        wiki_name[j] = name
+        if not search:
+            missing_name.append(name)
+
     global wiki_name
-    wiki_name = [None]*len(name_list)
+    wiki_name = [None] * len(name_list)
     global missing_name
     missing_name = []
     j = 0
     pool = mp.Pool(mp.cpu_count())
     for name in name_list:
-      pool.apply_async(norm, args=(name, j), callback=add_name)
-      j = j + 1
+        pool.apply_async(norm, args=(name, j), callback=add_name)
+        j = j + 1
     pool.close()
     pool.join()
     LOGGER.info(f'{len(missing_name)} people cant be found in wikipedia, They are:')
