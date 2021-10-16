2d838110e267f99ee96f64d5c71218984ee869da
Tim Grams <t.grams339@gmail.com>
2021-10-16 17:53:26 +0100

    moved function

--- src/models/evaluation.py
+++ src/models/evaluation.py
@@ -5,6 +5,7 @@ import pandas as pd
 import numpy as np
 import mimetypes
 import random
+import pickle
 from src.models.approximate_k_nearest_neighbors import ApproximateKNearestNeighbors
 from src.models.face_recognition import FaceRecognition
 from src.postprocessing.graph_postprocessing import extract_scenes
@@ -49,7 +50,8 @@ def evaluate_on_dataset(path: str = 'data/datasets/ytcelebrity',
         thumbnail_sample = list(entities) + additional_thumbnails
 
     # Model Training
-    hunter = FaceRecognition(thumbnail_list=thumbnail_sample, thumbnails_path=os.path.join(path_thumbnails, 'thumbnails'))
+    hunter = FaceRecognition(thumbnail_list=thumbnail_sample,
+                             thumbnails_path=os.path.join(path_thumbnails, 'thumbnails'))
     recognizer_model = ApproximateKNearestNeighbors()
 
     # Check if there are still any thumbnails missing
@@ -75,8 +77,9 @@ def evaluate_on_dataset(path: str = 'data/datasets/ytcelebrity',
         else:
             y = [hunter.recognize_image(path_to_file, recognizer_model)]
         per_file_results.append(get_evaluation_metrics(y, list(itertools.repeat([file['entities']], len(y))),
-                                                       missing_entities, single_true)) # it is for youtube faces dataset, because 'entities' in the csv file is 
-                                                                                       # string instead of 
+                                                       missing_entities,
+                                                       single_true))  # it is for youtube faces dataset, because 'entities' in the csv file is
+        # string instead of
         files.append(file)
         scores = np.add(scores, per_file_results[-1])
 
@@ -127,7 +130,7 @@ def get_evaluation_metrics(y_pred: list = None,
         scores[0] += Y_intersection_Z / Y_union_Z
 
         # Precision
-        Y = len(y_pred[index])        
+        Y = len(y_pred[index])
         scores[1] += Y_intersection_Z / Y
 
         # Recall
@@ -140,3 +143,60 @@ def get_evaluation_metrics(y_pred: list = None,
     scores = np.divide(scores, frame_count)
     LOGGER.info(f'Accuracy: {scores[0]}, Precision: {scores[1]}, Recall: {scores[2]}, f1: {scores[3]}')
     return scores
+
+
+def evaluate_youtube_faces_on_video_level(path: str = '.',
+                                          path_thumbnails: str = 'data/thumbnails', ):
+    """ Evaluate the model on youtube faces dataset on video level
+
+    Parameters
+    ----------
+    path: str, default = 'data/datasets/youtube-faces-db'
+        The Location of the dataset.
+
+    path_thumbnails: str, default = 'data/thumbnails'
+        The Location of the thumbnails.
+
+    """
+    data = pd.read_csv(os.path.join(path, 'information.csv'))
+
+    # Model Training
+    hunter = FaceRecognition(thumbnails_path=os.path.join(path_thumbnails, 'thumbnails_20'))
+    recognizer_model = ApproximateKNearestNeighbors()
+
+    # Evaluation
+    count_video = 0
+    correct_video = 0
+    entity_list = set(hunter.labels)
+    incorrect_video = []
+    for index, file in data.iterrows():
+        entity = file['entities']
+        if entity not in entity_list:
+            continue
+        path_to_video = os.path.join('..', *file['file'].split('/')[2:-2])
+        LOGGER.debug(path_to_video)
+        for video in os.listdir(path_to_video):
+            if os.path.isfile(os.path.join(path_to_video, video)):
+                continue
+            incorrect_video.append(os.path.join(path_to_video, video))
+            count_video = count_video + 1
+            correct = 0
+            frame_list = os.listdir(os.path.join(path_to_video, video))
+            for frame in frame_list:
+                y = hunter.recognize_image(os.path.join(path_to_video, video, frame), recognizer_model)
+                if entity in y:
+                    LOGGER.info(f'{entity} found in {frame}')
+                    correct = correct + 1
+                else:
+                    LOGGER.warning(f'{entity} not found in {frame}')
+                if np.divide(correct, len(frame_list)) > 0.1 or correct > 1:
+                    LOGGER.info(f'{entity} is recognized in {video}')
+                    correct_video = correct_video + 1
+                    incorrect_video.remove(os.path.join(path_to_video, video))
+                    break
+            LOGGER.info(f'correct video:{correct_video}, total video:{count_video}')
+        entity_list.remove(entity)
+    LOGGER.info(
+        f'There are {count_video} videos evaluated, in which {correct_video} are correctly recognized. The accuracy is {np.divide(correct_video, count_video)}')
+    with open('incorrect_videos.pickle', 'wb') as f:
+        f.write(pickle.dumps(incorrect_video))
